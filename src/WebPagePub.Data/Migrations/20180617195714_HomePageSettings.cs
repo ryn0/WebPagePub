92c226b39eb89bcaf41105556f496510cdbc6e28
ryn0 <3250001+ryn0@users.noreply.github.com>
2018-06-17 12:57:46 -0700

    home page settings

---
+++ src/WebPagePub.Data/Migrations/20180617195714_HomePageSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebPagePub.Data.Migrations
+{
+    public partial class HomePageSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHomePageSection",
+                table: "SitePageSection",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHomePage",
+                table: "SitePage",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHomePageSection",
+                table: "SitePageSection");
+
+            migrationBuilder.DropColumn(
+                name: "IsHomePage",
+                table: "SitePage");
+        }
+    }
+}
