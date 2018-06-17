a370e2a73ded26105fe106aab2c3d72bea04913e
ryn0 <ryanmichaelwilliams@gmail.com>
2018-06-17 14:41:12 -0700

    use .NET Core 2.1

--- CI/CI-Main.ps1
+++ CI/CI-Main.ps1
@@ -7,7 +7,7 @@ properties {
    # Build
    $BuildConfiguration          = "release"
    $DotNetRunTime               = "win7-x64"
-   $DotNetFramework             = "netcoreapp2.0"
+   $DotNetFramework             = "netcoreapp2.1"
    $msDeploy                    = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"    
    
    # Project paths
