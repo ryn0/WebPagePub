e5059823eb9977aa40a385528d99f215aab8e3d1
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-08-10 13:17:24 -0600

    link management and model changes

--- src/WebPagePub.WebApp/Models/LinkListModel.cs
+++ src/WebPagePub.WebApp/Models/LinkListModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
-
-namespace WebPagePub.Web.Models
+﻿namespace WebPagePub.Web.Models
 {
     public class LinkListModel
     {
         public List<LinkEditModel> Items { get; set; } = new List<LinkEditModel>();
+
+        public LinkEditModel? NewestLink { get; set; }
+
+        public List<char> UniqueFirstLetters { get; set; } = new List<char>();
     }
 }
