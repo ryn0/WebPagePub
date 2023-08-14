aca12c848bf3b9aea03f36deb512ba8d54960014
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-08-14 16:12:03 -0600

    model/ SEO updates

--- src/WebPagePub.WebApp/Controllers/LinkController.cs
+++ src/WebPagePub.WebApp/Controllers/LinkController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
+﻿using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using WebPagePub.Data.Constants;
