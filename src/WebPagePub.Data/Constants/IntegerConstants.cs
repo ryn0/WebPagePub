cf5fbdce2ed81283d6622950baee09d368f485ae
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-08-19 14:11:40 -0600

    increase cache time for pages

--- src/WebPagePub.Data/Constants/IntegerConstants.cs
+++ src/WebPagePub.Data/Constants/IntegerConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int PageSize = 10;
 
-        public const int PageCachingMinutes = 60;
+        public const int PageCachingMinutes = 180;
 
         public const int MaxAgeInSeconds = 1800;
 
