994197159806771d461598b071786f234ffb51e8
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-09-04 17:03:26 -0600

    refactor datetime helper with tests

---
+++ src/WebPagePub.ChatCommander/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,17 @@
+﻿namespace WebPagePub.ChatCommander.Helpers
+{
+    public class DateTimeHelpers
+    {
+        public static DateTime OffSetTime(DateTime now, int minutesOffsetForArticleMin, int minutesOffsetForArticleMax)
+        {
+            DateTime startDate = now.AddMinutes(minutesOffsetForArticleMin);
+            DateTime endDate = now.AddMinutes(minutesOffsetForArticleMax);
+
+            var randomTest = new Random();
+            TimeSpan timeSpan = endDate - startDate;
+            TimeSpan newSpan = new(0, randomTest.Next(0, (int)timeSpan.TotalMinutes + 1), 0); // +1 to include the upper bound
+
+            return startDate + newSpan;
+        }
+    }
+}
