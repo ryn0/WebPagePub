1898d4b5f32018d8d9729c153eb69ae24584fbb4
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-09-04 13:01:36 -0600

    unit tests on text helpers

--- src/WebPagePub.ChatCommander.UnitTests/HelpersTests/TextHelpersTests.cs
+++ src/WebPagePub.ChatCommander.UnitTests/HelpersTests/TextHelpersTests.cs
@@ -35,6 +35,9 @@ namespace WebPagePub.ChatCommander.UnitTests.HelpersTests
         [Theory]
         [InlineData("Sports > Ice Sports > Hockey", "Hockey")]
         [InlineData("States > Florida > Miami", "Miami")]
+        [InlineData("Trees > Palm Trees", "Palm Trees")]
+        [InlineData("Earth > USA > California > San Diego", "San Diego")]
+        [InlineData("San Diego", "San Diego")]
         public void ParseBreadcrumb_ValidDescription_IsRemovedOfUnwantedCharacters(string input, string expected)
         {
             var result = TextHelpers.ParseBreadcrumb(input);
@@ -49,6 +52,36 @@ namespace WebPagePub.ChatCommander.UnitTests.HelpersTests
         {
             var result = TextHelpers.StripHtml(input);
 
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("&lt;div&gt;Hello &amp; World&lt;/div&gt;", "<div>Hello & World</div>")]
+        [InlineData("&lt;p&gt;This is a paragraph.&lt;/p&gt;", "<p>This is a paragraph.</p>")]
+        [InlineData("No special characters.", "No special characters.")]
+        [InlineData("&lt;", "<")]
+        public void HtmlDecode_StringsToParse_DecodesHtmlEntitiesCorrectly(string input, string expected)
+        {
+            // Act
+            var result = TextHelpers.HtmlDecode(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("<div>Hello</div>", "Hello")]
+        [InlineData("<div><p>Hello & World</p></div>", "Hello & World")]
+        [InlineData("<strong>Bold</strong> and <em>italic</em>", "Bold and italic")]
+        [InlineData("No special characters.", "No special characters.")]
+        [InlineData("<a href=\"example.com\">Link</a>", "Link")]
+        [InlineData("&#60;div&#62;Encoded?&#60;/div&#62;", "&#60;div&#62;Encoded?&#60;/div&#62;")] // Not actual tags due to encoding
+        public void StripHtml_HtmlCharsAndNonHtmlChars_RemovesHtmlTags(string input, string expected)
+        {
+            // Act
+            var result = TextHelpers.StripHtml(input);
+
+            // Assert
             Assert.Equal(expected, result);
         }
     }
