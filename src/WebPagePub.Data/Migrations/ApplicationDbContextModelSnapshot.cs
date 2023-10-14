759a0a466ec23bf3c7e4f84125d9fe5cc790400d
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-10-14 07:52:12 -0600

    bot traffic

--- src/WebPagePub.Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/WebPagePub.Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace WebPagePub.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.11")
+                .HasAnnotation("ProductVersion", "7.0.12")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -346,6 +346,9 @@ namespace WebPagePub.Data.Migrations
                     b.Property<string>("IpAddress")
                         .HasMaxLength(75)
                         .HasColumnType("nvarchar(75)");
+
+                    b.Property<bool>("IsBot")
+                        .HasColumnType("bit");
 
                     b.Property<string>("RefererUrl")
                         .HasColumnType("nvarchar(max)");
