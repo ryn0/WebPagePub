1c83598ffcd5ca6ab83a8b329184eb5072c34997
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-11-14 11:31:31 -0700

    Referer header access

--- src/WebPagePub.WebApp/Controllers/LinkController.cs
+++ src/WebPagePub.WebApp/Controllers/LinkController.cs
@@ -64,7 +64,7 @@ namespace WebPagePub.Web.Controllers
             {
                 var ipAddress = context.Connection?.RemoteIpAddress?.ToString();
                 var url = request.GetDisplayUrl();
-                var referrer = context.Request.Headers["Referer"].ToString();
+                var referrer = context.Request.Headers.Referer.ToString();
                 var isBot = StringConstants.BotUserAgents.Any(bot => userAgent.Contains(bot));
 
                 await this.clickLogRepository.CreateAsync(new ClickLog()
