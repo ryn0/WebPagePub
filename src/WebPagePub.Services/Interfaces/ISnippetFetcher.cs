05f9c1b57eff2bcf51b3741473a693a67a312758
ryn0 <3250001+ryn0@users.noreply.github.com>
2025-09-25 15:12:55 -0400

    new page type for external ads

---
+++ src/WebPagePub.Services/Interfaces/ISnippetFetcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebPagePub.Services.Interfaces
+{
+    // ISnippetFetcher.cs
+    public interface ISnippetFetcher
+    {
+        Task<string> GetAsync(string url, TimeSpan cacheFor);
+    }
+}
